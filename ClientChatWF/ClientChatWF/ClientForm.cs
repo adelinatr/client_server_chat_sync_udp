f8ab32ab6f397aff13a37d76f75f51f8bc6c0bd6
AlexandruGabi47 <32970433+AlexandruGabi47@users.noreply.github.com>
2018-02-15 19:17:29 +0200

    Add files via upload

---
+++ ClientChatWF/ClientChatWF/ClientForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ClientChatWF
+{
+	public partial class ClientForm : Form
+	{
+		public ClientForm()
+		{
+			InitializeComponent();
+		}
+
+		private void buttonJoin_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				UdpClient client = new UdpClient(textBoxIPAdress.Text, int.Parse(textBoxPort.Text));
+            }
+			catch (Exception ex)
+			{
+				chatLog.Text += $"{DateTime.Now.ToString("h:mm:ss tt")}: {ex.Message}\r\n";
+				chatLog.ScrollBars = ScrollBars.Vertical;
+			}
+		}
+	}
+}
