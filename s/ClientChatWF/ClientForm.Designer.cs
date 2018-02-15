105b29cd39d446a251bc3c78950976eff695c56c
costin06 <35259218+costin06@users.noreply.github.com>
2018-02-15 20:45:19 +0200

    Client

---
+++ s/ClientChatWF/ClientForm.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace ClientChatWF
+{
+	partial class ClientForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.buttonJoin = new System.Windows.Forms.Button();
+            this.buttonQuit = new System.Windows.Forms.Button();
+            this.textBoxIPAdress = new System.Windows.Forms.TextBox();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chatLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Location = new System.Drawing.Point(569, 11);
+            this.statusLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(247, 48);
+            this.statusLabel.TabIndex = 2;
+            this.statusLabel.Text = "No connection";
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Location = new System.Drawing.Point(16, 542);
+            this.textBoxMessage.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxMessage.Multiline = true;
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(712, 36);
+            this.textBoxMessage.TabIndex = 3;
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSend.Location = new System.Drawing.Point(736, 542);
+            this.buttonSend.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(80, 37);
+            this.buttonSend.TabIndex = 4;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            // 
+            // buttonJoin
+            // 
+            this.buttonJoin.Location = new System.Drawing.Point(380, 11);
+            this.buttonJoin.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonJoin.Name = "buttonJoin";
+            this.buttonJoin.Size = new System.Drawing.Size(107, 43);
+            this.buttonJoin.TabIndex = 5;
+            this.buttonJoin.Text = "Join";
+            this.buttonJoin.UseVisualStyleBackColor = true;
+            this.buttonJoin.Click += new System.EventHandler(this.buttonJoin_Click);
+            // 
+            // buttonQuit
+            // 
+            this.buttonQuit.Location = new System.Drawing.Point(495, 11);
+            this.buttonQuit.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonQuit.Name = "buttonQuit";
+            this.buttonQuit.Size = new System.Drawing.Size(67, 43);
+            this.buttonQuit.TabIndex = 6;
+            this.buttonQuit.Text = "Quit";
+            this.buttonQuit.UseVisualStyleBackColor = true;
+            this.buttonQuit.Click += new System.EventHandler(this.buttonQuit_Click);
+            // 
+            // textBoxIPAdress
+            // 
+            this.textBoxIPAdress.Location = new System.Drawing.Point(16, 31);
+            this.textBoxIPAdress.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxIPAdress.Name = "textBoxIPAdress";
+            this.textBoxIPAdress.Size = new System.Drawing.Size(145, 22);
+            this.textBoxIPAdress.TabIndex = 7;
+            this.textBoxIPAdress.TextChanged += new System.EventHandler(this.textBoxIPAdress_TextChanged);
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(171, 31);
+            this.textBoxPort.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(59, 22);
+            this.textBoxPort.TabIndex = 8;
+            this.textBoxPort.TextChanged += new System.EventHandler(this.textBoxPort_TextChanged);
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(239, 31);
+            this.textBoxUsername.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(132, 22);
+            this.textBoxUsername.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 17);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Server IP Adress";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(181, 11);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Port";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(265, 11);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Username";
+            // 
+            // chatLog
+            // 
+            this.chatLog.AcceptsReturn = true;
+            this.chatLog.AcceptsTab = true;
+            this.chatLog.BackColor = System.Drawing.SystemColors.Control;
+            this.chatLog.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.chatLog.Location = new System.Drawing.Point(16, 63);
+            this.chatLog.Margin = new System.Windows.Forms.Padding(4);
+            this.chatLog.Multiline = true;
+            this.chatLog.Name = "chatLog";
+            this.chatLog.ReadOnly = true;
+            this.chatLog.Size = new System.Drawing.Size(799, 467);
+            this.chatLog.TabIndex = 16;
+            // 
+            // ClientForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(832, 592);
+            this.Controls.Add(this.chatLog);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxUsername);
+            this.Controls.Add(this.textBoxPort);
+            this.Controls.Add(this.textBoxIPAdress);
+            this.Controls.Add(this.buttonQuit);
+            this.Controls.Add(this.buttonJoin);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.statusLabel);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "ClientForm";
+            this.Text = "Chat";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Label statusLabel;
+		private System.Windows.Forms.TextBox textBoxMessage;
+		private System.Windows.Forms.Button buttonSend;
+		private System.Windows.Forms.Button buttonJoin;
+		private System.Windows.Forms.Button buttonQuit;
+		private System.Windows.Forms.TextBox textBoxIPAdress;
+		private System.Windows.Forms.TextBox textBoxPort;
+		private System.Windows.Forms.TextBox textBoxUsername;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox chatLog;
+	}
+}
+
