982a3e53af0444c7ead2b6f19aa4b83ce21abc36
David Galati <david.galati01@yahoo.com>
2018-02-15 19:52:37 +0200

    1

--- galhutunetwork1/galhutunetwork1/Program.cs
+++ galhutunetwork1/galhutunetwork1/Program.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Net.Sockets;
+using System.Net;
 namespace galhutunetwork1
 {
-    class Program
+    class Program   
     {
         static void Main(string[] args)
         {
+            //IPEndPoint localEP= new IPEndPoint
+
+            Socket s = new Socket(AddressFamily.InterNetwork,
+            SocketType.Dgram, ProtocolType.Udp);
+
+
+            IPAddress ipa = IPAddress.Parse("127.0.0.1");
+            Console.WriteLine("IP address is=" + ipa.ToString());
+
+            Console.WriteLine("IP address is=" + IPAddress.Loopback.ToString());
+            Console.WriteLine("Broadcast address is=" + IPAddress.Broadcast.ToString());
         }
     }
 }
