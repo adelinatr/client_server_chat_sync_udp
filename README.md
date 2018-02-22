b76547346d582f368aabf1fd4f664cb0b7843694
AlexandruGabi47 <constantinescu.alexandrugabi47@gmail.com>
2018-02-22 18:54:46 +0200

    added reworked project

---
+++ README.md
@@ -0,0 +1,29 @@
+Client Server Chat using UDP Sockets
+
+Server 
+Initially Console Application with argument <port>
+
+Create an UDP server socket that listens on 127.0.0.1
+  - create an IP address (localhost)
+  - choose the same port for client and server
+  - create a local endpoint that uses this address and port given as argument
+  - display a message ">>Server started"
+Create main loop logic that interprets messages like: join, quit and default(switch after message type)
+  - Create a remote endpoint that has any IP address and port 0
+  - The socket receives from the client the message, at first that could be of type: join, quit or other(use ReceiveFrom)
+  - Use Encoding.ASCII.GetString(data) for getting the message
+  - On the join case, the server display " >> Accepted connection from .."
+  - On the quit case, the server displays ">> Closed connection from .. " 
+  - On the default case, the server sends back the message received(use SendTo) and then breaks
+  - In the main loop, the server shows ">> Waiting"
+Add possibility to accept connections from multiple clients: use a list of clients(use ArrayList)
+Add windows form for the server and show the list of clients that are connected and port
+
+Client
+Windows forms application
+
+Create windows Forms that looks like preview client image
+  - On button Join event click: an UDP socket is created, a remote endpoint that uses the IP Address entered, message join is sent to the server. On a newly created thread, the client expects receiving messages
+  - On button Quit event click: message quit is sent to the server
+  - On button Send, the message in the text area, it is sent to the server. Create a method “Send" that represents the callback for a newly created thread
+  -  Window title: “Connected to <server name> on <port no>"
---
+++ client/client/ClientForm.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ClientChatWF
+{
+	public partial class ClientForm : Form
+	{
+		int oldHeight;
+		int oldWidth;
+		Socket client;
+		IPEndPoint remoteEp;
+		bool isConnected = false;
+
+		public ClientForm()
+		{
+			InitializeComponent();
+			textBoxIPAdress.Text = "127.0.0.1";
+			textBoxPort.Text = "9000";
+		}
+
+		private void buttonJoin_Click(object sender, EventArgs e)
+		{
+			if (!isConnected)
+			{
+                try
+				{
+					if (textBoxUsername.Text.Length != 0)
+					{
+						client = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+						remoteEp = new IPEndPoint(IPAddress.Parse(textBoxIPAdress.Text), int.Parse(textBoxPort.Text));
+						client.SendTo(Encoding.ASCII.GetBytes($"{textBoxUsername.Text}:join"), remoteEp);
+						isConnected = true;
+						statusLabel.Text = "Connected";
+					}
+				}
+				catch (Exception ex)
+				{
+					chatLog.Text += $"{ex.Message}\r\n";
+				}
+			}
+		}
+
+		private void buttonSend_Click(object sender, EventArgs e)
+		{
+			if (isConnected)
+			{
+				client.SendTo(Encoding.ASCII.GetBytes(textBoxMessage.Text), remoteEp);
+				chatLog.Text += $"{textBoxUsername.Text}:{ textBoxMessage.Text}\r\n";
+				textBoxMessage.Text = "";
+			}
+		}
+		private void buttonQuit_Click(object sender, EventArgs e)
+		{
+			if (isConnected)
+			{
+				client.SendTo(Encoding.ASCII.GetBytes($"{textBoxUsername.Text}:quit"), remoteEp);
+				isConnected = false;
+				statusLabel.Text = "Disconnected";
+			}
+		}
+
+		private void ClientForm_ResizeBegin(object sender, EventArgs e)
+		{
+			oldHeight = this.Height;
+			oldWidth = this.Width;
+		}
+
+		private void ClientForm_ResizeEnd(object sender, EventArgs e)
+		{
+			int heightDiff = oldHeight - this.Height;
+			int widthDiff = oldWidth - this.Width;
+
+			chatLog.Width -= widthDiff;
+			chatLog.Height -= heightDiff;
+
+			textBoxMessage.Top -= heightDiff;
+			textBoxMessage.Width -= widthDiff;
+
+			buttonSend.Top -= heightDiff;
+			buttonSend.Left -= widthDiff;
+
+			statusLabel.Width -= widthDiff;
+		}
+	}
+}
