1a6d56419667c8ccfb99345b583ae6bc2a0b9c69
adelinastepit <36442978+adelinastepit@users.noreply.github.com>
2018-02-25 21:26:47 +0200

    Additional features

--- server/server/Form1.Designer.cs
+++ server/server/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿using System.Windows.Forms;
+using System.Windows.Forms;
 
 namespace server
 {
@@ -30,34 +30,39 @@ namespace server
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("Client1");
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.Port = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Users = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox1
             // 
             this.textBox1.AllowDrop = true;
-            this.textBox1.Enabled = false;
             this.textBox1.Location = new System.Drawing.Point(6, 30);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(479, 233);
+            this.textBox1.Size = new System.Drawing.Size(351, 214);
             this.textBox1.TabIndex = 0;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Location = new System.Drawing.Point(12, 106);
+            this.panel1.Location = new System.Drawing.Point(12, 55);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(494, 282);
+            this.panel1.Size = new System.Drawing.Size(373, 271);
             this.panel1.TabIndex = 1;
             // 
             // label1
@@ -71,32 +76,91 @@ namespace server
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(47, 12);
+            this.textBox2.Location = new System.Drawing.Point(73, 12);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(75, 20);
             this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "127.0.0.1";
             // 
             // Port
             // 
             this.Port.AutoSize = true;
-            this.Port.Location = new System.Drawing.Point(15, 15);
+            this.Port.Location = new System.Drawing.Point(154, 15);
             this.Port.Name = "Port";
             this.Port.Size = new System.Drawing.Size(26, 13);
             this.Port.TabIndex = 3;
             this.Port.Text = "Port";
             // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(186, 12);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(75, 20);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "9000";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "IP Address";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(283, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listView1.Enabled = false;
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1});
+            this.listView1.Location = new System.Drawing.Point(403, 55);
+            this.listView1.Name = "listView1";
+            this.listView1.OwnerDraw = true;
+            this.listView1.Size = new System.Drawing.Size(148, 271);
+            this.listView1.TabIndex = 7;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "tt";
+            // 
+            // Users
+            // 
+            this.Users.AutoSize = true;
+            this.Users.Location = new System.Drawing.Point(400, 20);
+            this.Users.Name = "Users";
+            this.Users.Size = new System.Drawing.Size(65, 13);
+            this.Users.TabIndex = 8;
+            this.Users.Text = "Users online";
+            // 
             // Server
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(518, 400);
+            this.ClientSize = new System.Drawing.Size(692, 400);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.Users);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.Port);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.panel1);
             this.Name = "Server";
             this.Text = "Server";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_Closing);
-            this.Load += new System.EventHandler(this.Server_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
@@ -111,6 +175,12 @@ namespace server
         private System.Windows.Forms.Label label1;
         private TextBox textBox2;
         private Label Port;
+        private TextBox textBox3;
+        private Label label2;
+        private Button button1;
+        private ListView listView1;
+        private Label Users;
+        private ColumnHeader columnHeader1;
     }
 }
 
